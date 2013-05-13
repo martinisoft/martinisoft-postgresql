4449157e8809cd76153a68ca80635a509317f516
Aaron Kalin <akalin@martinisoftware.com>
2013-05-13 02:50:16 -0500

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+name             "martinisoft-postgresql"
+maintainer       "Aaron Kalin"
+maintainer_email "akalin@martinisoftware.com"
+license          "Apache 2.0"
+description      "Postgresql wrapper cookbook for Martini Software"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+supports "ubuntu"
+
+depends "postgresql"
