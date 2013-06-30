c8a0a8d884e33485076c3bb9f6515fabc345b848
Aaron Kalin <akalin@martinisoftware.com>
2013-06-30 15:32:25 -0500

    Bump cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "akalin@martinisoftware.com"
 license          "Apache 2.0"
 description      "Postgresql wrapper cookbook for Martini Software"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.2"
 
 supports "ubuntu"
 
