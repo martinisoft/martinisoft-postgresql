86a8820ab5afe9e80e96ac309e13ab46c8283210
Aaron Kalin <akalin@martinisoftware.com>
2014-08-19 03:55:48 -0500

    Use VERSION file

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'akalin@martinisoftware.com'
 license          'Apache 2.0'
 description      'Postgresql wrapper cookbook for Martini Software'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.0'
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
 
 supports 'ubuntu', '>= 12.04'
 
